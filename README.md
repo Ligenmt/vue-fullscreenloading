b826a40103e2366039974b1dac2b54a7b771cca4
ligen <ligenmt@163.com>
2020-02-25 01:29:45 +0800

    update md

--- README.md
+++ README.md
@@ -1,24 +1,74 @@
 # vue-fullscreenloading
 
-## Project setup
-```
-npm install
+基于Vue的全屏加载动画效果。[线上效果](https://ligenmt.github.io/vue-fullscreenloading/)
+
+[English document](https://github.com/ligenmt/vue-fullscreenloading/blob/master/README_EN.md)
+
+## 效果预览
+
+![Image text](https://ligenmt.github.io/resources/images/vue-fullscreenloading-auto.gif)
+
+
+## 赞赏
+
+请作者喝瓶可乐吧！(微信号：ligenmt)
+
+![微信扫一扫](https://ligenmt.github.io/resources/images/weixin.png)
+
+## 特别鸣谢
+
+- [二麻运维](http://www.ermazi.com/)
+
+## 前言
+
+这是基于vue的全屏加载动画效果，可以显示加载进度，有两种使用方式，一是能获取加载进度时手动控制加载进度效果，二是当不知道加载进度（如调用后端耗时接口）时由控件自动显示平滑的加载进度效果。
+
+## 使用说明
+
+### 手动控制
+
+```js
+    let loadingui = this.$loadingui({
+        type: 'default',
+        callback: () => {
+            // 加载结束回调
+            console.log("finish")
+        },
+    })
+    let percent = 0
+    let obj = setInterval(() => {
+        percent += 2
+        //控制加载进度显示效果
+        loadingui.loadPercent = percent
+        if (percent >= m) {
+            // 关闭
+            loadingui.close()
+            clearInterval(obj)
+        }
+    }, 50)
 ```
 
-### Compiles and hot-reloads for development
-```
-npm run serve
+### 自动控制
+
+```js
+    let loadingui = this.$loadingui({
+        type: 'auto',
+        callback: () => {
+            // 加载结束回调
+            console.log("finish")
+        },
+    })
+    let percent = 0
+    let obj = setInterval(() => {
+        percent += 1
+        if (percent >= 50) {
+            // 关闭时不会立刻结束动画效果，而是继续显示加载效果直至100%，显得更友好真实:)
+            loadingui.close()
+            clearInterval(obj)
+        }
+    }, 50)
 ```
 
-### Compiles and minifies for production
-```
-npm run build
-```
+## License
 
-### Lints and fixes files
-```
-npm run lint
-```
-
-### Customize configuration
-See [Configuration Reference](https://cli.vuejs.org/config/).
+[MIT](https://github.com/lin-xin/vue-manage-system/blob/master/LICENSE)
--- src/App.vue
+++ src/App.vue
@@ -1,111 +1,109 @@
 <template>
-  <div id="app">
-    <div class="header">
-      <h1 class="project-name">Vue-FullscreenLoading</h1>
-      <h2 class="project-desc">A lightweight fullscreen loading effect for vue</h2>
+    <div id="app">
+        <div class="header">
+            <h1 class="project-name">Vue-FullscreenLoading</h1>
+            <h2 class="project-desc">A lightweight fullscreen loading effect for vue</h2>
+        </div>
+        <button @click="clickDefault(50)">ShowLoadingDefault50%</button>
+        <button @click="clickDefault(100)">ShowLoadingDefault100%</button>
+        <button @click="clickAuto">ShowLoadingAuto</button>
     </div>
-    <button @click="clickDefault(50)">ShowLoadingDefault50%</button>
-    <button @click="clickDefault(100)">ShowLoadingDefault100%</button>
-    <button @click="clickAuto">ShowLoadingAuto</button>
-  </div>
 </template>
 
 <script>
 
 
-export default {
-  name: 'app',
-  components: {
+    export default {
+        name: 'app',
+        components: {},
 
-  },
+        methods: {
+            clickDefault(m) {
+                let loadingui = this.$loadingui({
+                    type: 'default',
+                    callback: () => {
+                        // eslint-disable-next-line no-console
+                        console.log("finish")
+                    },
+                })
+                let percent = 0
+                let obj = setInterval(() => {
+                    percent += 2
+                    loadingui.loadPercent = percent
+                    if (percent >= m) {
+                        loadingui.close()
+                        clearInterval(obj)
+                    }
+                }, 50)
+            },
 
-  methods: {
-    clickDefault(m) {
-      let loadingui = this.$loadingui({
-        type: 'default',
-        callback: () =>{
-          // eslint-disable-next-line no-console
-          console.log("finish")
+            clickAuto() {
+                let loadingui = this.$loadingui({
+                    type: 'auto',
+                    callback: () => {
+                        // eslint-disable-next-line no-console
+                        console.log("finish")
+                    },
+                })
+                let percent = 0
+                let obj = setInterval(() => {
+                    percent += 1
+                    if (percent >= 50) {
+                        loadingui.close()
+                        clearInterval(obj)
+                    }
+                }, 50)
+            },
         },
-      })
-      let percent = 0
-      let obj = setInterval(()=>{
-        percent += 2
-        loadingui.loadPercent = percent
-        if (percent >= m) {
-          loadingui.close()
-          clearInterval(obj)
-        }
-      }, 50)
-    },
-
-    clickAuto() {
-      let loadingui = this.$loadingui({
-        type: 'auto',
-        callback: () =>{
-          // eslint-disable-next-line no-console
-          console.log("finish")
-        },
-      })
-      let percent = 0
-      let obj = setInterval(()=>{
-        percent += 1
-        if (percent >= 50) {
-          loadingui.close()
-          clearInterval(obj)
-        }
-      }, 50)
-    },
-  },
-}
+    }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin: 0;
-  padding: 0;
-}
+    #app {
+        font-family: 'Avenir', Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+        margin: 0;
+        padding: 0;
+    }
 
-  .header {
-    color: #fff;
-    text-align: center;
-    background-color: #159957;
-    background-image: linear-gradient(120deg, #155799, #159957);
+    .header {
+        color: #fff;
+        text-align: center;
+        background-color: #159957;
+        background-image: linear-gradient(120deg, #155799, #159957);
 
-    padding: 5rem 6rem;
-  }
+        padding: 5rem 6rem;
+    }
 
-  .project-name {
-    font-size: 3.25rem;
-    margin-top: 0;
-    margin-bottom: 0.1rem;
-  }
+    .project-name {
+        font-size: 3.25rem;
+        margin-top: 0;
+        margin-bottom: 0.1rem;
+    }
 
-  .project-desc {
-    margin-bottom: 2rem;
-    font-weight: normal;
-    opacity: 0.7;
-  }
+    .project-desc {
+        margin-bottom: 2rem;
+        font-weight: normal;
+        opacity: 0.7;
+    }
 
-  button {
-    width: 220px;
-    height: 40px;
-    margin: 10px;
-    background-color: rgba(54, 181, 205, 1);
-    border: solid rgba(84, 84, 84, 1) 0px;
-    border-radius: 4px;
-    cursor: pointer !important;
-    color: white;
-    font-size: 15px;
-    transition: background-color 0.4s ease 0s;
-  }
+    button {
+        width: 220px;
+        height: 40px;
+        margin: 10px;
+        background-color: rgba(54, 181, 205, 1);
+        border: solid rgba(84, 84, 84, 1) 0px;
+        border-radius: 4px;
+        cursor: pointer !important;
+        color: white;
+        font-size: 15px;
+        transition: background-color 0.4s ease 0s;
+    }
 
-  button:hover {
-    background-color: #5BC9DE;
-  }
+    button:hover {
+        background-color: #5BC9DE;
+    }
 </style>
